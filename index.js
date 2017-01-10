ce931c2257eae05a7983143088c50047bcae35d5
samelie <sam@stinkdigital.com>
2017-01-10 15:41:24 -0800

    matching with app

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var path = require('path');
 var fs = require('fs');
 var _ = require('lodash');
 var Q = require('bluebird');
+var toBuffer = require('typedarray-to-buffer');
 var UUID = require('uuid');
 const fsWriteFile = Q.promisify(require("fs").writeFile);
 
@@ -21,6 +22,7 @@ Out folder
 
 //video fps for base64
 const IMAGE_FPS = 30
+const IMAGE_EXT = '.jpg'
 
 const pad = (str, max) => {
   str = str.toString();
@@ -47,6 +49,14 @@ class Mp4Record {
     }
   }
 
+  set imageExt(ext) {
+    this._imageExt = ext
+  }
+
+  get imageExt() {
+    return this._imageExt || IMAGE_EXT
+  }
+
   set saveDirectory(dir) {
     this._saveDir = dir
   }
@@ -57,18 +67,20 @@ class Mp4Record {
 
   addAudio(buffer) {
     this._audioBuffers.push(buffer)
+    console.log(this._audioBuffers.length);
   }
 
   addFrame(buffer) {
     this._videoBuffers.push(buffer)
   }
 
-  saveImage(base64Str, options) {
+  saveImage(base64Str, options = {}) {
     let {
       saveDir
     } = options
-    this._frameCounter++
-      let _name = pad(this._frameCounter, 12)
+    saveDir = saveDir || this.saveDirectory
+    this._frameCounter++;
+    let _name = pad(this._frameCounter, 12)
     var p = this._getImageSavePath(saveDir, _name)
     return this._writeFile(p, base64Str)
   }
@@ -80,14 +92,25 @@ class Mp4Record {
       saveDir,
       inputOptions,
       outputOptions,
+      clean,
       withBuffers
     } = options
+
+    saveDir = saveDir || this.saveDirectory
+    console.log(options);
+    console.log(saveDir);
+    console.log(this._audioBuffers);
     return new Q((yes, no) => {
       let _uuid = UUID.v4()
       let _audioStream = new stream.Readable()
       let _videoStream
       while (this._audioBuffers.length) {
-        _audioStream.push(this._audioBuffers.shift(),'binary')
+        let c = this._audioBuffers.shift()
+        console.log(typeof c);
+        if (typeof c === 'object') {
+          c = toBuffer(c)
+        }
+        _audioStream.push(toBuffer(c), 'binary')
       }
       _audioStream.push(null)
 
@@ -100,6 +123,7 @@ class Mp4Record {
       }
 
       let _a = path.join(saveDir, `${_uuid}_audio.m4a`)
+      let _atmp = path.join(saveDir, `${_uuid}_audio_tmp.m4a`)
       let _v = path.join(saveDir, `${_uuid}_video.mp4`)
       let _p = path.join(saveDir, `${_uuid}.mp4`)
 
@@ -108,7 +132,7 @@ class Mp4Record {
           '-c:a libfaac',
           '-vn'
         ])
-        .output(_a)
+        .output(_atmp)
         .on('start', (commandLine) => {
           console.log('Spawned Ffmpeg with command: ' + commandLine);
         })
@@ -125,74 +149,103 @@ class Mp4Record {
           console.log("Finished audio encode");
           this._audioBuffers.length = 0
 
-          if (withBuffers) {
-            this._encodeVideoWithBuffers(_videoStream, options, _v, (videoPath) => {
-              this._videoBuffers.length = 0
-              this._mergeAudioVideo(_p, _a, _v)
-                .then(() => {
-                  console.log(`Success ${_p}`);
-                  yes(_p)
-                })
-            })
-          } else {
-            this._writeFiles(saveDir, _uuid, this._videoBuffers)
+          if (inputOptions || outputOptions) {
+            this._trimAudio(_atmp, _a, inputOptions, outputOptions)
               .then(() => {
-                this._encodeFromBase64(saveDir, _v)
-                  .then(() => {
-                    this._mergeAudioVideo(_p, _a, _v)
-                      .then(() => {
-                        console.log(`Success ${_p}`);
-                        yes(_p)
-                      })
-                  })
+                fs.unlinkSync(_atmp)
+                if (withBuffers) {
+                  this._onAudioEncodeCompleteSaveBuffer(_videoStream, options, _v, _a, _p, yes, no)
+                } else {
+                  this._onAudioEncodeCompleteWriteFile(saveDir, _uuid, _a, _v, _p, this._videoBuffers, yes, no)
+                }
               })
+          } else {
+            //rename to correct
+            fs.renameSync(_atmp, _a)
+            if (withBuffers) {
+              this._onAudioEncodeCompleteSaveBuffer(_videoStream, options, _v, _a, _p, yes, no)
+            } else {
+              this._onAudioEncodeCompleteWriteFile(saveDir, _uuid, _a, _v, _p, this._videoBuffers, yes, no)
+            }
           }
         })
         .run();
     })
   }
 
-  _encodeVideoWithBuffers(stream, options, outputPath, cb) {
+  _onAudioEncodeCompleteSaveBuffer(videoStream, options, videoPath, audioPath, finalPath, yes, no) {
+    return this._encodeVideoWithBuffers(videoStream, options, videoPath)
+      .then(videoPath => {
+        this._videoBuffers.length = 0
+        return this._mergeAudioVideo(finalPath, audioPath, videoPath)
+          .then(() => {
+            console.log(`Success ${finalPath}`);
+            yes(finalPath)
+          })
+      })
+      .catch(err => {
+        no(err)
+      })
+  }
+
+  _onAudioEncodeCompleteWriteFile(saveDir, uuid, audioPath, videoPath, finalPath, videoBuffers, yes, no) {
+    return this._writeFiles(saveDir, uuid, videoBuffers)
+      .then(() => {
+        return this._encodeFromBase64(saveDir, videoPath)
+          .then(() => {
+            return this._mergeAudioVideo(finalPath, audioPath, videoPath)
+              .then(() => {
+                yes(finalPath)
+              })
+          })
+      })
+      .catch(err => {
+        no(err)
+      })
+  }
+
+  _encodeVideoWithBuffers(stream, options, outputPath) {
     let {
       width,
       height
     } = options
-
-    ffmpeg(stream)
-      .inputFormat('rawvideo')
-      .inputOptions([
-        '-framerate 30', //should be 30... but 15 works
-        `-s ${width}x${height}`,
-        '-c:v rawvideo',
-        '-pix_fmt', 'rgba',
-      ])
-      .size(`${width}x${height}`)
-      .format('mp4')
-      .outputOptions([
-        '-c:v libx264',
-        '-framerate 30',
-        '-an'
-      ])
-      .output(outputPath)
-      .on('start', (commandLine) => {
-        console.log('Spawned Ffmpeg with command: ' + commandLine);
-      })
-      .on('stderr', (stderrLine) => {})
-      .on('error', (err, stdout, stderr) => {
-        console.log('Cannot process video: ' + err.message);
-        console.log(stdout);
-        console.log(stderr);
-        no(err)
-      })
-      .on('end', () => {
-        cb(outputPath)
-      })
-      .run()
+    return new Q((yes, no) => {
+      ffmpeg(stream)
+        .inputFormat('rawvideo')
+        .inputOptions([
+          '-framerate 30', //should be 30... but 15 works
+          `-s ${width}x${height}`,
+          '-c:v rawvideo',
+          '-pix_fmt', 'rgba',
+        ])
+        .size(`${width}x${height}`)
+        .format('mp4')
+        .outputOptions([
+          '-c:v libx264',
+          '-framerate 30',
+          '-an'
+        ])
+        .output(outputPath)
+        .on('start', (commandLine) => {
+          console.log('Spawned Ffmpeg with command: ' + commandLine);
+        })
+        .on('stderr', (stderrLine) => {})
+        .on('error', (err, stdout, stderr) => {
+          console.log('Cannot process video: ' + err.message);
+          console.log(stdout);
+          console.log(stderr);
+          no(err)
+        })
+        .on('end', () => {
+          yes(outputPath)
+        })
+        .run()
+    })
   }
 
   _getImageSavePath(dir, name, uuid) {
     let _n = uuid ? `${name}${uuid}` : name
-    return path.join(dir, `${_n}.png`)
+    return path.join(dir, `${_n}${this.imageExt}`)
   }
 
   _writeFile(p, base64Str) {
@@ -214,8 +267,9 @@ class Mp4Record {
 
   _encodeFromBase64(dir, output, encodeOptions) {
 
-    let _p = `"${path.join(dir,"*.png")}"`
+      let _p = `"${path.join(dir,`*${this.imageExt}`)}"`
     let _c = `ffmpeg -framerate ${IMAGE_FPS} -pattern_type glob -i ${_p} -y -c:v libx264 -framerate ${IMAGE_FPS} -an ${output}`
+    console.log(_c);
     return new Q((yes, no) => {
       let _cmd = exec(_c, (code, stdout, stderr) => {
         /*
@@ -252,6 +306,19 @@ class Mp4Record {
         .run();
     })
 
+  }
+
+  _trimAudio(input, output, inputOptions = [], outputOptions = []) {
+    let _c = `ffmpeg ${inputOptions.toString()}  -i ${input} -y  -vn -c:a copy ${outputOptions.toString()} ${output}`
+    console.log(_c);
+    return new Q((yes, no) => {
+      let _cmd = exec(_c, (code, stdout, stderr) => {
+        /*
+        The output is in stderr for some reason
+        */
+        yes(output)
+      });
+    })
   }
 
   destroy() {
